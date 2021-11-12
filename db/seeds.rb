ee7e20cdff405290183fea0857ac0b7fd4978c83
Alessia Garbi <lalebi@live.com>
2021-11-12 11:52:56 +0100

    change platform in gemfile.lock

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,3 +35,27 @@ Movie.create!(
   poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
   rating: 7.0
 )
+
+# require 'open-uri'
+# require 'json'
+
+# puts "Cleaning up database..."
+# Movie.destroy_all
+# puts "Database cleaned"
+
+# url = "http://tmdb.lewagon.com/movie/top_rated"
+# 10.times do |i|
+#   puts "Importing movies from page #{i + 1}"
+#   movies = JSON.parse(open("#{url}?page=#{i + 1}").read)['results']
+#   movies.each do |movie|
+#     puts "Creating #{movie['title']}"
+#     base_poster_url = "https://image.tmdb.org/t/p/original"
+#     Movie.create(
+#       title: movie['title'],
+#       overview: movie['overview'],
+#       poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+#       rating: movie['vote_average']
+#     )
+#   end
+# end
+# puts "Movies created"
